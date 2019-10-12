ef9e73db53036b0fb859ba2ac2f1162bdea860ca
CP02A <itiscp02a@gmail.com>
2019-10-12 14:40:44 +0200

    Added Telegram Chat Listener

--- NodeJs/api/github/app_api_github.js
+++ NodeJs/api/github/app_api_github.js
@@ -2,34 +2,71 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-router.post('/sendmessage', function(req, res){
+var hist = [];
+
+fetch('https://api.telegram.org/bot966625238:AAEwp5LYgYYBXeyz_sLcgwZ2rsdgYDMy6Eg/getUpdates').then(
+    function(u){ return u.json();}
+).then(
+    function(json){
+        json.result.forEach(i => {
+            hist.push(i.update_id);
+        });
+    }
+);
+
+setInterval(() => {
+    fetch('https://api.telegram.org/bot966625238:AAEwp5LYgYYBXeyz_sLcgwZ2rsdgYDMy6Eg/getUpdates').then(
+        function(u){ return u.json();}
+    ).then(
+        function(json){
+            json.result.forEach(i => {
+                if(!hist.includes(i.update_id)){
+                    console.log("new message!! " + i.message.text);
+                    if(i.message.text === "hi"){
+                        send(i.message.chat.id, 'hi!')
+                    }
+                    hist.push(i.update_id);
+                }
+            });
+        }
+    )
+}, 3000);
+
+router.post('/sendmessage', (req, res) => {
     const header = req.headers;
     const msg = req.body;
     res.send('success!');
-    if(header["x-github-event"] === "issues")
-        if(msg.action === "opened")
-            send("[" + msg.repository.name + "] " + msg.issue.user.login + " opened an issue called \"" + msg.issue.title + "\"");
-    if(msg.action === "closed")
-        send("[" + msg.repository.name + "] " + msg.issue.user.login + " closed an issue called \"" + msg.issue.title + "\"");
-    else if(header["x-github-event"]  === "push") {
+    if (header["x-github-event"] === "issues") {
+        if (msg.action === "opened")
+            send(729494108, "[" + msg.repository.name + "] " + msg.issue.user.login + " opened an issue called \"" + msg.issue.title + "\"");
+        if (msg.action === "closed")
+            send(729494108, "[" + msg.repository.name + "] " + msg.issue.user.login + " closed an issue called \"" + msg.issue.title + "\"");
+    } else if(header["x-github-event"]  === "push") {
         var temp = "";
         msg.commits.forEach((i) => {
             i.message.split("\n").forEach((o) => {
                 temp += "\n - " + o;
             });
         });
-        send("[" + msg.repository.name + "] " + msg.pusher.name + " pushed the following changes:" + temp);
+        send(729494108, "[" + msg.repository.name + "] " + msg.pusher.name + " pushed the following changes:" + temp);
     } else if(header["x-github-event"]  === "pull_request")
-        send("[" + msg.repository.name + "] " + msg.pull_request.user.login + " opened a pull request titled \"" + msg.pull_request.title + "\"!");
+        send(729494108, "[" + msg.repository.name + "] " + msg.pull_request.user.login + " opened a pull request titled \"" + msg.pull_request.title + "\"!");
     else if(header["x-github-event"]  === "fork")
-        send("[" + msg.repository.name + "] " + msg.forkee.owner.login + " forked!");
+        send(729494108, "[" + msg.repository.name + "] " + msg.forkee.owner.login + " forked!");
     else
-        send("An unknown request has been sent:\n"+JSON.stringify(req.body));
+        send(729494108, "An unknown request has been sent:\n"+JSON.stringify(req.body));
 });
 
-function send(text){
+router.post('/update', (req, res) => {
+    const header = req.headers;
+    const msg = req.body;
+    res.send('success!');
+    console.log(msg);
+});
+
+function send(id, text){
     console.log("Somebody sent " + text + "!");
-    fetch('https://api.telegram.org/bot966625238:AAEwp5LYgYYBXeyz_sLcgwZ2rsdgYDMy6Eg/sendMessage?chat_id=729494108&text=' + text);
+    fetch('https://api.telegram.org/bot966625238:AAEwp5LYgYYBXeyz_sLcgwZ2rsdgYDMy6Eg/sendMessage?chat_id=' + id + '&text=' + text);
 }
 
 module.exports = router;
